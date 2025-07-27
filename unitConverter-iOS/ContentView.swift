e819403dad5d2db28d202c8aee26fcec6648f4d6
sougatoroy3 <sougatoroy007@gmail.com>
2025-07-27 14:22:57 +0530

    Variables assigned

--- unitConverter-iOS/ContentView.swift
+++ unitConverter-iOS/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue : String = ""
+    @State private var inputUnit : String = ""
+    @State private var outputUnit : String = ""
+    @State private var outputValue : String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
     }
 }
 
